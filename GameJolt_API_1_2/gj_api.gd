801860e160a93b6078c456da491cc48dd3d5e12b
Jonathan <big.server.error@gmail.com>
2018-12-07 18:53:31 +0100

    Sorted API calls according to the public gamejolt api documentation (https://gamejolt.com/game-api/doc)

--- GameJolt_API_1_2/gj_api.gd
+++ GameJolt_API_1_2/gj_api.gd
@@ -37,27 +37,45 @@ const PARAMETERS = {
 	data_keys_get     = ['&username=', '&user_token=', '&pattern='],
 	time_fetch        = []
 }
-const API_COMMAND = { 
-	user_auth         = ['users/auth/',               PARAMETERS.user_auth]
-	user_fetch        = ['users/',                    PARAMETERS.user_fetch]
-	friends           = ['friends/',                  PARAMETERS.friends_fetch]
+
+# https://gamejolt.com/game-api/doc
+const API_COMMAND = {
+
+#	Data-store          Manipulate items in a cloud-based data storage.
+	data_fetch        = ['data-store/',               PARAMETERS.data_fetch]
+	data_keys_get     = ['data-store/get-keys/',      PARAMETERS.data_keys_get]
+	data_remove       = ['data-store/remove/',        PARAMETERS.data_remove]
+	data_set          = ['data-store/set/',           PARAMETERS.data_set]
+	data_update       = ['data-store/update/',        PARAMETERS.data_update]
+
+#	Time	            Get the server's time.
+	time_fetch        = ['time/',                     PARAMETERS.time_fetch]
+
+#	Scores	            Manipulate scores on score tables.
+	scores_add        = ['scores/add/',               PARAMETERS.scores_add]
+	scores_fetch_rank = ['scores/get_rank/',          PARAMETERS.scores_fetch_rank]
+	scores_fetch      = ['scores/',                   PARAMETERS.scores_fetch]
+	tables_fetch      = ['scores/tables/',            PARAMETERS.tables_fetch]
+
+#	Sessions	        Set up sessions for your game.
 	session_open      = ['sessions/open/',            PARAMETERS.sessions]
 	session_ping      = ['sessions/ping/',            PARAMETERS.sessions]
-	session_close     = ['sessions/close/',           PARAMETERS.sessions]
 	session_check     = ['sessions/check/',           PARAMETERS.sessions]
+	session_close     = ['sessions/close/',           PARAMETERS.sessions]
+
+#	Trophies	        Manage trophies for your game.
 	trophy_fetch      = ['trophies/',                 PARAMETERS.trophy_fetch]
 	trophy_add        = ['trophies/add-achieved/',    PARAMETERS.trophy_add_remove]
 	trophy_remove     = ['trophies/remove-achieved/', PARAMETERS.trophy_add_remove]
-	scores_fetch      = ['scores/',                   PARAMETERS.scores_fetch]
-	scores_add        = ['scores/add/',               PARAMETERS.scores_add]
-	scores_fetch_rank = ['scores/get_rank/',          PARAMETERS.scores_fetch_rank]
-	tables_fetch      = ['scores/tables/',            PARAMETERS.tables_fetch]
-	data_fetch        = ['data-store/',               PARAMETERS.data_fetch]
-	data_set          = ['data-store/set/',           PARAMETERS.data_set]
-	data_update       = ['data-store/update/',        PARAMETERS.data_update]
-	data_remove       = ['data-store/remove/',        PARAMETERS.data_remove]
-	data_keys_get     = ['data-store/get-keys/',      PARAMETERS.data_keys_get]
-	time_fetch        = ['time/',                     PARAMETERS.time_fetch]
+
+#	Users	            Access user-based features.
+	user_auth         = ['users/auth/',               PARAMETERS.user_auth]
+	user_fetch        = ['users/',                    PARAMETERS.user_fetch]
+
+#	Friends	            List a user's friends.
+	friends           = ['friends/',                  PARAMETERS.friends_fetch]
+
+#	Batch	            Merge multiple API calls into one request.
 }
 
 signal error(error)
@@ -76,20 +94,49 @@ func get_user_token():
 	return token_cache
 
 	
-func auth_user(token, username):
-	# checks if the credentials are correct and chaches them
-	gj_api(API_COMMAND.user_auth, [token, username])
-	username_cache = username
-	token_cache = token
+func fetch_data(key, username=null, token=null):
+	# returns data stored for this game (and user if set)
+	gj_api(API_COMMAND.data_fetch, [key, username, token])
+
+func get_data_keys(username=null, token=null, pattern=null):
+	# returns all keys or limited to a given user or by a pattern
+	gj_api(API_COMMAND.data_keys_get, [username, token, pattern])
+
+func remove_data(key, username=null, token=null):
+	# removes data for this game (and user if set)
+	gj_api(API_COMMAND.data_remove, [key, username, token])	
 	
-func fetch_user(username=null, id=null):
-	# returns a user's data - only username or user_id is required
-	gj_api(API_COMMAND.user_fetch, [username, id])
+func set_data(key, data, username=null, token=null):
+	# stores data for this game (and user if set)
+	gj_api(API_COMMAND.data_set, [key, data, username, token])
 	
-func fetch_friends():
-	# returns all friends of the cached user
-	gj_api(API_COMMAND.friends, [username_cache, token_cache])
+func update_data(key, operation, value, username=null, token=null):
+	# operates on already set data: add / subtract / multiply / divide / append / prepend
+	gj_api(API_COMMAND.data_update, [key, operation, value, username, token])
+
 	
+func fetch_time():
+	# returns the time of the GameJolt server
+	gj_api(API_COMMAND.time_fetch, [])
+
+	
+func add_score(score, sort, username=null, token=null, guest=null, table_id=null):
+	# adds a score, pass either username/token or guest. uses the main table if no id provided
+	gj_api(API_COMMAND.scores_add, [score, sort, username, token, guest, table_id])
+	
+func fetch_score_rank(sort, table_id=null):
+	# retrieves the rank of the nearest score
+	gj_api(API_COMMAND.scores_fetch_rank, [sort, table_id])
+
+func fetch_scores(username=null, token=null, limit=null, table_id=null, better_than=null, worse_than=null):
+	# returns scores, all arguments are optional, but pass either username/token or guest; better_than or worse_than
+	gj_api(API_COMMAND.scores_fetch, [username, token, limit, table_id, better_than, worse_than])
+	
+func fetch_tables():
+	# fetches the ids of all tables for this game
+	gj_api(API_COMMAND.tables_fetch, [])
+
+
 func open_session():
 	# opens a game session for the cached user
 	gj_api(API_COMMAND.session_open, [username_cache, token_cache])
@@ -98,13 +145,14 @@ func ping_session():
 	# call at least every 120s to keep session opened
 	gj_api(API_COMMAND.session_ping, [username_cache, token_cache])
 	
-func close_session():
-	# closes the game session for the cached user
-	gj_api(API_COMMAND.session_close, [username_cache, token_cache])
-	
 func check_session():
 	# checks if there is an open session in this game for the cached user
 	gj_api(API_COMMAND.session_check, [username_cache, token_cache])
+	
+func close_session():
+	# closes the game session for the cached user
+	gj_api(API_COMMAND.session_close, [username_cache, token_cache])
+
 	
 func fetch_trophy(achieved=null, trophy_ids=null):
 	# returns (only achieved) trophies (with given id / ids) for the cached user
@@ -117,47 +165,23 @@ func set_trophy_achieved(trophy_id):
 func remove_trophy_achieved(trophy_id):
 	# removes teh specified trophy from the cached user
 	gj_api(API_COMMAND.trophy_remove, [username_cache, token_cache, trophy_id])
+
+
+func auth_user(token, username):
+	# checks if the credentials are correct and chaches them
+	gj_api(API_COMMAND.user_auth, [token, username])
+	username_cache = username
+	token_cache = token
 	
-func fetch_scores(username=null, token=null, limit=null, table_id=null, better_than=null, worse_than=null):
-	# returns scores, all arguments are optional, but pass either username/token or guest; better_than or worse_than
-	gj_api(API_COMMAND.scores_fetch, [username, token, limit, table_id, better_than, worse_than])
-	
-func add_score(score, sort, username=null, token=null, guest=null, table_id=null):
-	# adds a score, pass either username/token or guest. uses the main table if no id provided
-	gj_api(API_COMMAND.scores_add, [score, sort, username, token, guest, table_id])
-	
-func fetch_score_rank(sort, table_id=null):
-	# retrieves the rank of the nearest score
-	gj_api(API_COMMAND.scores_fetch_rank, [sort, table_id])
-	
-func fetch_tables():
-	# fetches the ids of all tables for this game
-	gj_api(API_COMMAND.tables_fetch, [])
-	
-func fetch_data(key, username=null, token=null):
-	# returns data stored for this game (and user if set)
-	gj_api(API_COMMAND.data_fetch, [key, username, token])
-	
-func set_data(key, data, username=null, token=null):
-	# stores data for this game (and user if set)
-	gj_api(API_COMMAND.data_set, [key, data, username, token])
-	
-func update_data(key, operation, value, username=null, token=null):
-	# operates on already set data: add / subtract / multiply / divide / append / prepend
-	gj_api(API_COMMAND.data_update, [key, operation, value, username, token])
-	
-func remove_data(key, username=null, token=null):
-	# removes data for this game (and user if set)
-	gj_api(API_COMMAND.data_remove, [key, username, token])
-	
-func get_data_keys(username=null, token=null, pattern=null):
-	# returns all keys or limited to a given user or by a pattern
-	gj_api(API_COMMAND.data_keys_get, [username, token, pattern])
-	
-func fetch_time():
-	# returns the time of the GameJolt server
-	gj_api(API_COMMAND.time_fetch, [])
-		return url
+func fetch_user(username=null, id=null):
+	# returns a user's data - only username or user_id is required
+	gj_api(API_COMMAND.user_fetch, [username, id])
+
+
+func fetch_friends():
+	# returns all friends of the cached user
+	gj_api(API_COMMAND.friends, [username_cache, token_cache])
+
 
 
 # use like: callback(this, '_my_callback').some_api_func(...)
@@ -198,6 +222,8 @@ func compose_url(api_command, parameters):
 	if debug:
 		print(url + '&signature=' + s)
 		return url + '&signature=' + s
+	
+	return url
 
 
 func _on_HTTPRequest_request_completed( result, response_code, headers, body ):
